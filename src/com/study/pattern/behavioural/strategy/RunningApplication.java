e9317ef84bfa6021c9e15676dd1f71b96b377dab
SuprasannaBhaumik <suprasanna.bhaumik86@gmail.com>
2018-09-18 13:18:01 +0530

    definition added for the pattern
    The strategy pattern is a behavioral design pattern that allows you to decide which course of action a program should take, based on a specific context during runtime. You encapsulate two different algorithms inside two classes, and decide at runtime which strategy you want to go with.

--- src/com/study/pattern/behavioural/strategy/RunningApplication.java
+++ src/com/study/pattern/behavioural/strategy/RunningApplication.java
@@ -1,10 +1,17 @@
 package com.study.pattern.behavioural.strategy;
 
+
+//  The Strategy pattern is a behavioral design pattern,
+//  that allows you to decide which course of action a program should take, 
+//  based on a specific context during runtime. 
+//  You encapsulate two different algorithms inside two classes, 
+//  and decide at runtime which strategy you want to go with.
 public class RunningApplication {
 
 	public static void main(String[] args) {
 		
 		CustomStrategy runningStrategy = new AtheleteRunStrategyImpl();
+		//CustomStrategy runningStrategy = new BabyRunStrategyImpl();
 		
 		RunningContext context = new RunningContext();
 		context.setRunner(runningStrategy);
